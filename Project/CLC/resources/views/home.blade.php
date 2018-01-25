07fb8373ea5cc04ef81198ef69053b1cee12abd6
Connor Low <clow2@my.gcu.edu>
2018-01-24 19:39:43 -0700

    Milestone 1 - (unstable)

---
+++ Project/CLC/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<?php
+/*
+version 0.1
+
+Connor
+CST-256
+January 19, 2018
+This assignment was completed in collaboration with Connor Low, Ali Cooper.
+We used source code from the following websites to complete this assignment: N/A
+*/
+?>
+
+<html>
+@php include('resources/assets/snippets/html_head.php');
+@endphp
+<head>
+    <link rel="stylesheet" href="public/css/styles.css">
+    <title>Create Account</title>
+</head>
+<body>
+<div class="container">
+    <div class="pageTitle">
+        <h1>Home</h1>
+    </div>
+    <div id="welcomeHome">
+        <p>Sign-in successful.</p>
+    </div>
+    <a href="logout">Sign out</a>
+</div>
+</body>
+</html>
