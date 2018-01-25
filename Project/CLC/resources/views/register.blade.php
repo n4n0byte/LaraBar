2fb226cb1c382596482eb86d8321e54fce090bfc
Ali Cooper <coopnukem@gmail.com>
2018-01-24 21:37:23 -0700

    added registration inputs

--- Project/CLC/resources/views/register.blade.php
+++ Project/CLC/resources/views/register.blade.php
@@ -25,13 +25,26 @@ We used source code from the following websites to complete this assignment: N/A
     <div id="registerForm" class="basicForm" >
         <form method="POST" action="create_account">
             <input type="hidden" name="_token" value="@php echo csrf_token() @endphp">
+            
             <div class="form-horizontal">
                 <h3>Email</h3>
-                <input max="200" name="email" type=email title="Email" required>
+                <input max="200" name="email" type="email" title="Email" required>
             </div>
+            
+            <div class="form-horizontal">
+                <h3>First Name</h3>
+                <input max="200" name="email" type="text" title="Email" required>
+            </div>
+            
+            <div class="form-horizontal">
+                <h3>Last Name</h3>
+                <input max="200" name="email" type="text" title="Email" required>
+            </div>
+            
+            
             <div class="form-horizontal">
                 <h3>Password</h3>
-                <input max="200" name="password" title="Password" type="password">
+                <input max="200" name="password" title="Password" type="password" required>
             </div>
             <div class="form-submit">
                 <input title="Sign In" type="submit">
