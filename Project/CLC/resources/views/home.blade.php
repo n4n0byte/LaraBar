ca3dac03e409ea1df934a32c78e03508891d8217
Connor Low <clow2@my.gcu.edu>
2018-02-02 13:07:26 -0700

    home page

---
+++ Project/CLC/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<?php
+/*
+version 0.2
+
+Connor
+CST-256
+January 24, 2018
+This assignment was completed in collaboration with Connor Low, Ali Cooper.
+We used source code from the following websites to complete this assignment: N/A
+*/
+?>
+
+<html>
+@php include('resources/assets/snippets/html_head.php');
+@endphp
+<head>
+    <link rel="stylesheet" href="public/css/styles.css">
+    <title>Create Account</title>
+</head>
+<body>
+<div class="container">
+    <div class="pageTitle">
+        <h1>Home</h1>
+    </div>
+    <div id="welcomeHome">
+        <p>Sign-in successful.</p>
+        <p>@php echo $email @endphp</p>
+    </div>
+    <a href="./logout">Sign out</a>
+</div>
+</body>
+</html>
