f3e15893e64d29f779295cad9fc5bef3607ca8ef
Ali Cooper <coopnukem@gmail.com>
2018-02-04 19:21:35 -0700

    updated edit view

--- Project/CLC/resources/views/edit_profile.blade.php
+++ Project/CLC/resources/views/edit_profile.blade.php
@@ -54,14 +54,14 @@ $userProfile = $data['userProfile'];
 
         <div class="form-group">
             <label class="label">Education</label>
-            <textarea class="form-control" name="" >
+            <textarea class="form-control" name="education" ></textarea>
         </div>
 
         <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <label for="exampleInputPassword1">Location</label>
+            <input type="text" class="form-control">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary btn-block">Submit</button>
     </form>
 
 </div>
