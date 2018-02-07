c59b06322b5067f13bd5b573e41b8a90fe1307e2
Ali Cooper <coopnukem@gmail.com>
2018-02-07 04:03:45 -0700

    styled admin page properly

--- Project/CLC/resources/views/components/adminTable.blade.php
+++ Project/CLC/resources/views/components/adminTable.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-bordered table-hover">
+<table class="table table-bordered table-hover" style="background-color: rgba(255,255,255,.8);">
         <tr>
             <th>ID</th>
             <th>EMAIL</th>
