100a2e739090745c35fecb52956e13d8cbe3a3fd
Ali Cooper <coopnukem@gmail.com>
2018-02-06 19:14:04 -0700

    added blade template for profile page

---
+++ Project/CLC/resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+{{--
+version 2.1
+
+Ali, Connor
+CST-256
+February 4, 2018
+This assignment was completed in collaboration with Connor Low, Ali Cooper.
+We used source code from the following websites to complete this assignment: N/A
+--}}
+
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/resources/assets/css/bootstrap.css">
+    <script src="/resources/assets/js/jquery-3.3.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="/resources/assets/js/bootstrap.js"></script>
+    <script src="/resources/assets/js/bootstrap.bundle.js"></script>
+    <script src="/resources/assets/js/particles.js"></script>
+    <script src="/resources/assets/js/tether.js"></script>
+
+    <title>@yield('title')</title>
+</head>
+
+<body>
+@yield('navbar')
+@yield('body')
+</body>
+
+</html>
--- Project/CLC/resources/views/profile.blade.php
+++ Project/CLC/resources/views/profile.blade.php
@@ -11,23 +11,10 @@ $user = $data['user'];
 $userProfile = $data['userProfile'];
 ?>
 
-<html>
+@extends('layouts.master')
+@section('title','Profile')
 
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/resources/assets/css/bootstrap.css">
-    <script src="/resources/assets/js/jquery-3.3.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="/resources/assets/js/bootstrap.js"></script>
-    <script src="/resources/assets/js/bootstrap.bundle.js"></script>
-    <script src="/resources/assets/js/particles.js"></script>
-    <script src="/resources/assets/js/tether.js"></script>
-
-    <title>Create Account</title>
-</head>
-
-<body>
-
+@section('navbar')
 <ul class="nav nav-tabs justify-content-end ">
     <li class="nav-item">
         <a class="nav-link" href="home">Home</a>
@@ -42,8 +29,9 @@ $userProfile = $data['userProfile'];
     </li>
 
 </ul>
+@endsection
 
-
+@section('body')
 <div class="container mx-lg-auto mt-5">
 
     <div class="card-deck">
@@ -111,6 +99,4 @@ $userProfile = $data['userProfile'];
     </div>
 
 </div>
-</body>
-
-</html>
+@endsection
