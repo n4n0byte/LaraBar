bb969f8037c6110c46f4a780d82eef532388f8a9
Ali Cooper <coopnukem@gmail.com>
2018-02-20 01:55:03 -0700

    update testing done

--- Project/CLC/resources/views/components/profile/editEducation.blade.php
+++ Project/CLC/resources/views/components/profile/editEducation.blade.php
@@ -1,4 +1,4 @@
-@component('components.form',['method' => 'POST', 'action' => '/CLC/profile/updateEducation'])
+@component('components.form',['method' => 'POST', 'action' => '/CLC/profile/editEducation'])
     <input type="hidden" name="post-id" value="{{$id}}">
     @component('components.editTextInput',['id' => 'institution', 'label' => 'University/Institution', 'data' => $institution,
                                                  'name' => 'institution'])
