eea6f9509da7e22880c582a6bad00949c0f75c27
Connor Low <clow2@my.gcu.edu>
2018-02-20 22:34:49 -0700

    Fixed job post validation error

--- Project/CLC/app/Http/Controllers/AdminController.php
+++ Project/CLC/app/Http/Controllers/AdminController.php
@@ -122,7 +122,6 @@ class AdminController extends Controller
 
         // Define rules
         $rules = [
-            'id' => 'Required|Numeric',
             'title' => 'Required|Between:4,20',
             'author' => 'Required|Between:5,50',
             'location' => 'Required|Between:2,40',
