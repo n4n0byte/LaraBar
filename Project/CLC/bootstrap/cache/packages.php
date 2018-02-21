d0a5ee7e9c99475b1c9dc12ad14d290a85c8461b
Ali Cooper <coopnukem@gmail.com>
2018-02-20 23:02:04 -0700

    added ddl

---
+++ Project/CLC/bootstrap/cache/packages.php
@@ -0,0 +1,30 @@
+<?php return array (
+  'appstract/laravel-blade-directives' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Appstract\\BladeDirectives\\BladeDirectivesServiceProvider',
+    ),
+  ),
+  'appstract/laravel-bootstrap-components' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Appstract\\BootstrapComponents\\BootstrapComponentsServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+);
