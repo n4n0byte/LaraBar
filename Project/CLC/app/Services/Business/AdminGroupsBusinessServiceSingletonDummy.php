7dcac6eab306e34986186aad34c3c589e7f7322a
Ali Cooper <coopnukem@gmail.com>
2018-03-03 00:05:35 -0700

    filled out Admin Group Singleton Dummy

--- Project/CLC/app/Services/Business/AdminGroupsBusinessServiceSingletonDummy.php
+++ Project/CLC/app/Services/Business/AdminGroupsBusinessServiceSingletonDummy.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Business;
 
+use App\Model\GroupModel;
 use App\Services\BusinessInterfaces\IAdminGroupsBusinessService;
 
 class AdminGroupsBusinessServiceSingletonDummy implements IAdminGroupsBusinessService {
@@ -10,6 +11,9 @@ class AdminGroupsBusinessServiceSingletonDummy implements IAdminGroupsBusinessSe
     private $groups = array();
     private $users = array();
 
+    /**
+     * @return IAdminGroupsBusinessService
+     */
     public static function getInstance(): IAdminGroupsBusinessService {
 
         if (self::$instance == null){
@@ -19,29 +23,66 @@ class AdminGroupsBusinessServiceSingletonDummy implements IAdminGroupsBusinessSe
         return self::$instance;
     }
 
+    /**
+     * AdminGroupsBusinessServiceSingletonDummy constructor.
+     * initializes instance and provides dummy data
+     */
     private function __construct() {
-        //todo add stuff
+        $userSvc = new UserBusinessService(session()->get('user'));
+
+        // store all users from db
+        $users = $userSvc->listUsers();
+        $groupName = "group ";
+
+        // creates groups and stores users in each
+        for ($i = 0; $i < 10; $i++){
+            $group = new GroupModel($groupName . $i, "Lorum Ipsum", "Description",
+                "Owner Name",$i);
+            array_push($this->groups, $group);
+        }
     }
 
 
-
+    /**
+     * @param array $details
+     * @return bool
+     */
     public function createGroup(array $details): bool {
-        // TODO: Implement createGroup() method.
+        array_push($this->groups, new GroupModel("New Group","None",
+                                            "None","None","99"));
         return true;
     }
 
+    /**
+     * @param $groupId
+     * @return bool
+     */
     public function deleteGroup($groupId): bool {
-        // TODO: Implement deleteGroup() method.
+        unset($this->groups,$groupId);
         return true;
     }
 
+    /**
+     * @return array
+     */
     public function listAllGroups(): array {
-        // TODO: Implement listAllGroups() method.
-        return [];
+        return $this->groups;
     }
 
+    /**
+     * @param array $details
+     * @return bool
+     */
     public function editGroupDetails(array $details): bool {
-        // TODO: Implement editGroupDetails() method.
+        $id = $details["id"];
+        $name = $details["name"];
+        $description = $details["description"];
+        $owner = $details["owner"];
+
+        $item = array_search($id, $this->groups);
+        $item->setName($name);
+        $item->setDescription($description);
+        $item->setOwner($owner);
         return true;
     }
 
