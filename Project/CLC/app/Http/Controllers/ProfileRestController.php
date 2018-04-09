cf241d242187d94341dc20536be7336065bde953
Connor Low <clow2@my.gcu.edu>
2018-04-08 20:46:31 -0700

    More error catching

--- Project/CLC/app/Http/Controllers/ProfileRestController.php
+++ Project/CLC/app/Http/Controllers/ProfileRestController.php
@@ -12,25 +12,29 @@ class ProfileRestController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
+        try {
+            $userSvc = new UserProfileBusinessService();
+            $result = $userSvc->getProfileData($id);
+            $statusCode = 200;
+            $message = "success";
 
-        $userSvc = new UserProfileBusinessService();
-        $result = $userSvc->getProfileData($id);
-        $statusCode = 200;
-        $message = "success";
+            if ($result == null) {
+                $statusCode = 404;
+                $message = "No profile found";
+            }
 
-        if ($result == null){
-            $statusCode = 404;
-            $message = "No profile found";
+            $dto = new DTO($statusCode, $message, $result);
+
+            return json_encode($dto);
+        } catch (Exception $e) {
+            $dto = new DTO(500, "It broke", []);
+            return json_encode($dto);
         }
-
-        $dto = new DTO($statusCode,$message,$result);
-
-        return json_encode($dto);
 
     }
 
